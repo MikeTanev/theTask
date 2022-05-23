09efd12af56a00456ba6e43b0f63a26aec10c56f
MikeTanev <mihail.strashimirov.tanev@gmail.com>
2022-05-23 09:57:25 +0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, Component, ViewChild, VERSION } from '@angular/core';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, ViewChild, VERSION } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatInput } from '@angular/material/input';
+
 
 @Component({
   selector: 'my-app',
@@ -17,109 +19,108 @@ export class AppComponent {
   constructor() {
     const myData: repos[] = [
       {
-      id: 101,
       'repoName': 'oss-review-toolkit',
       'description': 'A suite of tools to assist with reviewing Open Source Software dependencies.',
       'progLang': 'kotlin',
-      'stars': 6,
+      'stars': '6',
       'repoURL': 'https://github.com/bosch-io/oss-review-toolkit'
       },
       {
-      id: 102,
       'repoName': 'sono',
       'description': 'Hono project',
       'progLang': 'Java',
-      'stars': 7,
+      'stars': '7',
       'repoURL': 'https://github.com/bosch-io/hono'
       },
       {
-      id: 103,
       'repoName': 'ditto-clients',
       'description': 'Client SDKs for the Eclipse Ditto Project https://eclipse.org/ditto/',
       'progLang': 'Java',
-      'stars': 2,
+      'stars': '2',
       'repoURL': 'https://github.com/bosch-io/ditto-clients'
       },
       {
-      id: 104,
       'repoName': 'californium',
       'description': '',
       'progLang': 'Java',
-      'stars': 2,
+      'stars': '2',
       'repoURL': 'https://github.com/bosch-io/californium'
       },
       {
-      id: 105,
       'repoName': 'ditto',
       'description': '',
       'progLang': 'Java',
-      'stars': 11,
+      'stars': '11',
       'repoURL': 'https://github.com/bosch-io/ditto'
       },
       {
-      id: 106,
       'repoName': 'tinydtls',
       'description': 'Eclipse tinydtls',
       'progLang': 'C',
-      'stars': 1,
+      'stars': '1',
       'repoURL': 'https://github.com/bosch-io/tinydtls'
       },
       {
-      id: 107,
       'repoName': 'iot-things-examples',
       'description': '',
       'progLang': 'JavaScript',
-      'stars': 59,
+      'stars': '59',
       'repoURL': 'https://github.com/bosch-io/iot-things-examples'
       },
       {
-      id: 108,
       'repoName': 'business-partner-agent-chart',
       'description': '',
       'progLang': 'Smarty',
-      'stars': 0,
+      'stars': '0',
       'repoURL': 'https://github.com/bosch-io/business-partner-agent-chart'
       },
       {
-      id: 109,
       'repoName': 'kanto',
       'description': '',
       'progLang': 'HTML',
-      'stars': 0,
+      'stars': '0',
       'repoURL': 'https://github.com/bosch-io/kanto'
       },
       {
-      id: 110,
       'repoName': 'aries-vcx',
       'description': 'Implementation of Aries protocols in Rust with wrappers for NodeJS, Java, Android and iOS.',
       'progLang': 'Rust',
-      'stars': 0,
+      'stars': '0',
       'repoURL': 'https://github.com/bosch-io/aries-vcx'
       },
       {
-      id: 111,
       'repoName': 'aries-rfcs',
       'description': 'Hyperledger Aries is infrastructure for blockchain-rooted, peer-to-peer interactions',
       'progLang': 'Python',
-      'stars': 181,
+      'stars': '181',
       'repoURL': 'https://github.com/bosch-io/aries-vcx'
       }
     ];
 
     this.dataSource = new MatTableDataSource(myData);
   }
-    displayedColumns = ['repoName',	'description',	'progLang',	'stars',	'repoURL'];
+  
+  displayedColumns = ['repoName',	'description',	'progLang',	'stars',	'repoURL'];
 
-    ngAfterViewInit() {    
-      this.dataSource.sort = this.sort;
-    }
+  ngAfterViewInit() {    
+    this.dataSource.sort = this.sort;
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+  /*applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // This is to remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }*/
 }
 
 export interface repos {
-  id: number;
   repoName: string;
   description: string;
   progLang: string;
-  stars: number;
+  stars: string;
   repoURL: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSortModule} from '@angular/material/sort';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatTableModule, BrowserAnimationsModule, MatSortModule ],
+  imports:      [ BrowserModule, FormsModule, MatTableModule, BrowserAnimationsModule, MatSortModule, MatInputModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
